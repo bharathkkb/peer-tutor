ad722b1a21cd0a867cb377d2365cab88c3a2af05
Bharath Baiju <bbaiju@vmware.com>
2018-11-04 01:44:27 -0800

    added params for pytest

--- peer-tutor-api/test_api.py
+++ peer-tutor-api/test_api.py
@@ -6,32 +6,32 @@ from flex.core import load, validate_api_call
 import pprint
 import pytest
 import json
-
-testAPIBasePath = "http://localhost:5000/test/api"
-
-# helper for validate swagger spec
+import sys
 
 
-def validateSwagger():
+def validateSwagger(url):
+    testAPIBasePath = "{}/test/api".format(url)
     validate_spec_url(testAPIBasePath + '/swagger.json')
 
 # check connection to server
 
 
-def test_case_connection():
+def test_case_connection(url):
+    testAPIBasePath = "{}/test/api".format(url)
     response = requests.get(testAPIBasePath + '/hello')
     assert response.status_code == 200
 
 # pytest for validating swagger schema
 
 
-def test_validateSwagger():
-    assert validateSwagger() == None
+def test_validateSwagger(url):
+    assert validateSwagger(url) == None
 
 # helper for validate hello schema
 
 
-def hello_schema(schema):
+def hello_schema(url, schema):
+    testAPIBasePath = "{}/test/api".format(url)
     response = requests.get(testAPIBasePath + '/hello')
     validate_api_call(schema, raw_request=response.request,
                       raw_response=response)
@@ -39,14 +39,16 @@ def hello_schema(schema):
 # pytest for validating hello schema
 
 
-def test_hello_schema():
+def test_hello_schema(url):
+    testAPIBasePath = "{}/test/api".format(url)
     schema = flex.load(testAPIBasePath + '/swagger.json')
-    assert hello_schema(schema) == None
+    assert hello_schema(url, schema) == None
 
 # pytest for validating data returned by /hello endpoint
 
 
-def test_hello_data():
+def test_hello_data(url):
+    testAPIBasePath = "{}/test/api".format(url)
     response = requests.get(testAPIBasePath + '/hello')
     data = json.loads(response.content)
     print(data)
