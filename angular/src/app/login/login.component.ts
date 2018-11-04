d5b16d28c08d6eeadf5b5f508817334b135b408a
firen777 <hivemindac1995@gmail.com>
2018-11-03 23:21:11 -0700

    bare bone as fk login page

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private loginUsername = "";
+  private loginPassword = "";
+
+  private testUsername = "";
+  private testPassword = "";
+  
+  /**
+   * try to log in
+   */
+  tryLogin() {
+    if (this.checkUser(this.loginUsername).pass) {this.testUsername = this.loginUsername;}
+    else {this.testUsername = this.checkUser(this.loginUsername).msg}
+    if (this.checkPassword(this.loginPassword).pass) {this.testPassword = this.loginPassword;}
+    else {this.testPassword = this.checkPassword(this.loginPassword).msg}
+    
+  }
+
+
+  /**
+   * check password and see if pass
+   * @param p password to be checked
+   */
+  checkPassword(p:string):{pass:boolean, msg?:string}{
+    if (p.length<8) return {pass:false, msg:"Too short"};
+    return {pass:true};
+  }
+
+  /**
+   * check password and see if pass
+   * @param p password to be checked
+   */
+  checkUser(p:string):{pass:boolean, msg?:string}{
+    if (p.length<=0) return {pass:false, msg:"No name"};
+    return {pass:true};
+  }
+
   constructor() { }
 
   ngOnInit() {
