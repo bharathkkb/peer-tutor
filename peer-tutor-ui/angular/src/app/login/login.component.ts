4cff73a388371655f0f9a126efc063f943b85b26
firen777 <hivemindac1995@gmail.com>
2018-11-06 19:11:42 -0800

    login WIP

--- peer-tutor-ui/angular/src/app/login/login.component.ts
+++ peer-tutor-ui/angular/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/_services';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -6,48 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  private loginUsername = "";
-  private loginPassword = "";
-
-  private testUsername = "";
-  private testPassword = "";
-  
-  /**
-   * try to log in
-   */
-  tryLogin() {
-    if (this.checkUser(this.loginUsername).pass) {this.testUsername = this.loginUsername;}
-    else {this.testUsername = this.checkUser(this.loginUsername).msg}
-    if (this.checkPassword(this.loginPassword).pass) {this.testPassword = this.loginPassword;}
-    else {this.testPassword = this.checkPassword(this.loginPassword).msg}
+    private loginUsername:string;
+    private loginPassword:string;
+    private returnUrl:string;
+    private activatedRoute: ActivatedRoute;
+    private submitted = false;
     
-  }
+    private loginError = false;
+    private loginErrorMsg:Object;
 
+    /**
+     * try to log in
+     */
+    tryLogin() {
+        this.submitted = true;
+        this.authenticationService.login(this.loginUsername, this.loginPassword)
+            .pipe(first())
+            .subscribe(
+                data=>{this.router.navigate([this.returnUrl])},
+                error=>{this.loginError=true; this.loginErrorMsg=error; console.log(error)}
+            );
+        
+        console.log(this.loginUsername + " " + this.loginPassword);
 
-  /**
-   * check password and see if pass
-   * @param p password to be checked
-   */
-  checkPassword(p:string):{pass:boolean, msg?:string}{
-    if (p.length<8) return {pass:false, msg:"Too short"};
-    return {pass:true};
-  }
+    }
 
-  /**
-   * check password and see if pass
-   * @param p password to be checked
-   */
-  checkUser(p:string):{pass:boolean, msg?:string}{
-    if (p.length<=0) return {pass:false, msg:"No name"};
-    return {pass:true};
-  }
+    constructor(
+        private authenticationService: AuthenticationService, 
+        private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        // logout
+        this.authenticationService.logout();
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
+    }
 }
 
 /*
