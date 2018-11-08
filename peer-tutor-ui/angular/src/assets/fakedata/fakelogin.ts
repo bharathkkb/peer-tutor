e9bf202a12c3c9ac549c88d7c1713daeaaef47e6
firen777 <hivemindac1995@gmail.com>
2018-11-07 21:53:54 -0800

    fake user added field id

--- peer-tutor-ui/angular/src/assets/fakedata/fakelogin.ts
+++ peer-tutor-ui/angular/src/assets/fakedata/fakelogin.ts
@@ -1,31 +1,38 @@
 const fakeUsers = [
     {
         "username": "user1",
-        "password": "password1"
+        "password": "password1",
+        "id": 1
     },
     {
         "username": "user2",
-        "password": "password2"
+        "password": "password2",
+        "id": 2
     },
     {
         "username": "user3",
-        "password": "password3"
+        "password": "password3",
+        "id": 3
     },
     {
         "username": "user4",
-        "password": "password4"
+        "password": "password4",
+        "id": 4
     },
     {
         "username": "user5",
-        "password": "password5"
+        "password": "password5",
+        "id": 5
     },
     {
         "username": "user6",
-        "password": "password6"
+        "password": "password6",
+        "id": 6
     },
     {
         "username": "user7",
-        "password": "password7"
+        "password": "password7",
+        "id": 7
     }
 ]
 
