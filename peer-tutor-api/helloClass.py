d45ab48758cdc961fe59c932ecf0e791a333e9f5
YANPENG LI <yanpengli@YANPENGs-MacBook-Pro.local>
2018-11-08 20:18:03 -0800

    add classes

--- peer-tutor-api/helloClass.py
+++ peer-tutor-api/helloClass.py
@@ -1,5 +1,10 @@
 import json
-
+from student import Student
 
 def testHello():
     return json.loads('{"hello": "hello"}')
+
+#def get_student_id(id):
+#    data = mongo_handler().get_data("from student get {id_:{0}}".format(id))
+#    return Student(data)
+
--- peer-tutor-api/student.py
+++ peer-tutor-api/student.py
@@ -21,6 +21,7 @@ class Student:
         """
         return student's id
         """
+
         return self.student_id
 
     def get_name(self):
@@ -64,71 +65,4 @@ class Student:
         add a schedule
         """
         return True
-
-
-class Peer:
-    """
-    Returns a ```peer``` object with the given peer Id, peer_ratings.
-
-    """
-    def __init__(self, student_id, name, enrolled_classes, schedule, meetings, peer_id, peer_ratings):
-        super().__init__(student_id, name, enrolled_classes, schedule, meetings)
-        self.peer_id = student_id
-        self.peer_ratings = peer_ratings
-        print("A peer object is created.")
-
-        def __str__(self):
-            """
-            Prints the details of the peer.
-            """
-            return self.student_id + " " + self.name + " " + self.enrolled_classes.__str__ + " " + self.schedule.__str__ + " " + self.meetings.__str__ + " "+ self.peer_id + " " + self.peer_ratings
-
-        def get_peer_id(self):
-            """
-            return student's peer id
-            """
-            return self.peer_id
-
-        def get_peer_ratings(self):
-            """
-            return peer_ratings
-            """
-            return self.peer_ratings
-
-class Tutor:
-    """
-    Returns a ```tutor``` object with the given tutor Id, tutor_ratings.
-
-    """
-    def __init__(self, student_id, name, enrolled_classes, schedule, meetings, tutor_id, tutor_ratings):
-        super().__init__(student_id, name, enrolled_classes, schedule, meetings)
-        self.tutor_id = tutor_id
-        self.tutor_ratings = tutor_ratings
-        print("A tutor object is created.")
-
-        def __str__(self):
-            """
-            Prints the details of the tutor.
-            """
-            ###test
-            return self.student_id + " " + self.name + " " + self.enrolled_classes.__str__ + " " + self.schedule.__str__ + " " + self.meetings.__str__ + " " + self.tutor_id + " " + self.tutor_ratings
-
-        def set_student(self):
-            """
-            set
-            """
-            return True
-
-        def get_tutor_id(self):
-            """
-            return tutor_id
-            """
-            return self.tutor_id
-
-        def get_tutor_ratings(self):
-            """
-            return tutor_ratings
-            """
-            return self.tutor_ratings
-
 
