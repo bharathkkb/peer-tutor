02d82bcdfed71e3bb807a275a7a557374a4ed196
firen777 <hivemindac1995@gmail.com>
2018-11-10 16:01:39 -0800

    pass all initial tests

--- peer-tutor-ui/angular/src/app/_guards/auth.guard.spec.ts
+++ peer-tutor-ui/angular/src/app/_guards/auth.guard.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       providers: [AuthGuard]
     });
   });
