79a1cb67133d04366301f321bbbaf8f8a2a1c4d8
firen777 <hivemindac1995@gmail.com>
2018-11-11 21:23:54 -0800

    home page w/ class data WIP

--- peer-tutor-ui/angular/src/app/home-page/home-page.component.ts
+++ peer-tutor-ui/angular/src/app/home-page/home-page.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ClassDataService } from '../_services';
+import { UniClass } from '../_models/uniclass';
+
+/**
+ * Sumary of a UniClass
+ */
+class ClassSum {
+  "_id": string;
+  "class-name": string;
+  "start-dates": string;
+  "end-dates": string;
+  "days": string;
+  "instructor": string;
+  "location": string;
+  "start-time": string;
+  "end-time": string;
+  "title": string;
+  "section": string;
+}
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +26,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  classes$: ClassSum[];
+
+  constructor( private classDataService:ClassDataService ) { }
 
   ngOnInit() {
+    this.classDataService.getAll().subscribe(
+      classes => { 
+        this.classes$ = classes.map((c:UniClass)=>{
+          let result:ClassSum = {
+            "_id": c._id,
+            "class-name": c["class-name"],
+            "start-dates": c.dates.substring(0, 8),
+            "end-dates": c.dates.substring(9, 17),
+            "days": c.days,
+            "instructor": c.instructor,
+            "location": c.location,
+            "start-time": c.time.substring(0, 4),
+            "end-time": c.time.substring(5, 10),
+            "title": c.title,
+            "section": c.section,
+          }
+          return result
+        }); 
+      }
+    )
   }
 
 }
