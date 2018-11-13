3866f9b8af859dad1b3d2482a762de781bb49218
YANPENG LI <yanpengli@YANPENGs-MBP.hsd1.ca.comcast.net>
2018-11-12 21:31:04 -0800

    added meeting_driver.py

---
+++ peer-tutor-api/meeting_driver.py
@@ -0,0 +1,72 @@
+import json
+from mongoDriver import mongoDriver
+
+from bson import json_util, ObjectId
+from meeting import Meeting
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+# returns one meeting with that meeting_id
+
+
+def getMeetingById(meetingID):
+    query = dict()
+    query["meeting_id"] = meetingID
+    return json.loads(json_util.dumps(mongoDriver().getFindOne("peer-tutor-db", "meeting", query)))
+
+# returns a list of meetings with peer name matches with the given string
+# loosely matches for example "lif" would return lifeng
+
+def getMeetingsByPeer(peerID):
+    query = dict()
+    query["peer_id"] = peerID
+    return json.loads(json_util.dumps(mongoDriver().getFind("peer-tutor-db", "meeting", query)))
+
+
+# returns a list of meetings with tutor name matches with the given string
+
+def getMeetingsByTutor(tutorID):
+    query = dict()
+    query["tutor"] = tutorID
+    return json.loads(json_util.dumps(mongoDriver().getFind("peer-tutor-db", "meeting", query)))
+
+# inserts new meeting
+# if a meeting with the given meeting_id is found then the data is updated
+
+
+def putMeeting(meetingData):
+    # check if old meeting exists in db
+    if getMeetingById(meetingData["meeting_id"]):
+        updateMeeting = getMeetingById(meetingData["meeting_id"])
+        # make new meeting with the same meeting id
+        newMeetingData = Meeting(
+            updateMeeting["meeting_id"], meetingData["peer_id"], meetingData["tutor_id"])
+        # update the meeting info in db
+        mongoDriver().updateDict("peer-tutor-db", "meeting",
+                                 updateMeeting, newMeetingData.get_json())
+        # return the latest meeting and 200 status code
+        return getMeetingById(meetingData["meeting_id"]), 200
+    else:
+        # make a new meeting
+        m = Meeting(meetingData["meeting_id"], meetingData["peer_id"], meetingData["tutor_id"])
+        # add the new meeting to db
+        mongoDriver().putDict("peer-tutor-db", "meeting", m.get_json())
+        # return new meeting obj with 201 status code
+        return getMeetingById(meetingData["meeting_id"]), 201
+
+def getAllMeeting(studentID):
+
+
+
+# just for testing purpose
+if __name__ == "__main__":
+    m = dict()
+    m["meeting_id"] = "06"
+    m["peer_id"] = "00011"
+    m["tutor_id"] = "10001"
+    print(putMeeting(m))
--- peer-tutor-api/mongoSeed.py
+++ peer-tutor-api/mongoSeed.py
@@ -6,18 +6,19 @@ if "peer-tutor-db" in dblist:
     client.drop_database("peer-tutor-db")
 print("Starting Seeding")
 mydb = client["peer-tutor-db"]
+
+
 studentCol = mydb["student"]
-student1 = {"name": "Lifeng", "student_id": "02",
-            "username": "lifeng@gmail.com", "password": "pass123"}
-student2 = {"name": "Albert", "student_id": "04",
-            "username": "albert@gmail.com", "password": "pass123"}
-student3 = {"name": "Sheldon", "student_id": "06",
-            "username": "sheldon@gmail.com", "password": "sheldon123"}
+student1 = {"name": "Lifeng", "student_id": "02"}
+student2 = {"name": "Albert", "student_id": "04"}
 studentCol.insert_one(student1)
 studentCol.insert_one(student2)
-studentCol.insert_one(student3)
 
-meetingCol = mydb["meetings"]
-meeting1 = {"name": "meeting1", "meeting_id": "02"}
+meetingCol = mydb["meeting"]
+meeting1 = {"meeting_id": "01", "peer_id": "00001", "tutor_id": "10001"}
+meeting2 = {"meeting_id": "02", "peer_id": "00002", "tutor_id": "10002", }
 meetingCol.insert_one(meeting1)
+meetingCol.insert_one(meeting2)
+
+
 print("inserted")
