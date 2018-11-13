bd1130cb6aa07fa1ef556331925a7c1a7d4e4260
firen777 <hivemindac1995@gmail.com>
2018-11-13 02:00:25 -0800

    init, note and url for machine on LAN

--- peer-tutor-ui/angular/src/environments/environment.ts
+++ peer-tutor-ui/angular/src/environments/environment.ts
@@ -3,10 +3,33 @@
 // The list of file replacements can be found in `angular.json`.
 
 //TODO: change the API URL to actual backend!
+/**
+ * apiUrl need to be configured for every new run!
+ */
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4200'
+  // apiUrl: 'http://localhost:4200',
+  apiUrl: 'http://192.168.0.100:5000',
 };
+
+/*
+Note to self:
+To run Docker on Windows 7:
+download Docker toolbox etc.
+run Docker terminal
+run commands
+change setting: 
+  VirtualBox -> 
+  docker machine -> 
+  setting -> 
+  network -> 
+  adapter with NAT mode -> 
+  port forwarding -> 
+  map 5000 to 5000 w/ host 127.0.0.1 and local IP address
+
+Configure firewall to allow port 5000 inbound traffic
+
+ */
 
 /*
  * For easier debugging in development mode, you can import the following file
