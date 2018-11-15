3fda0945965bd495950c45be2e6759f74691cf18
firen777 <hivemindac1995@gmail.com>
2018-11-14 23:55:58 -0800

    service for local/session storage draft

---
+++ peer-tutor-ui/angular/src/app/_services/local-storage.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  /**A general method to be used to save data into local storage
+   * 
+   * @param key key for the data
+   * @param data data object to be saved into local storage
+   */
+  setLocalStorage(key:string, data:any){
+    try {
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      console.error('Error saving to localStorage', e);
+    }
+  }
+
+  /**A general method to get data from local storage
+   * 
+   * @param key key for the data
+   */
+  getLocalStorage(key: string):any {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (e) {
+      console.error('Error getting data from localStorage', e);
+      return null;
+    }
+  }
+
+  /**A general method to be used to save data into session storage
+   * 
+   * @param key key for the data
+   * @param data data object to be saved into session storage
+   */
+  setSessionStorage(key:string, data:any){
+    try {
+      sessionStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      console.error('Error saving to sessionStorage', e);
+    }
+  }
+
+  /**A general method to get data from session storage
+   * 
+   * @param key key for the data
+   */
+  getSessionStorage(key: string):any {
+    try {
+      return JSON.parse(sessionStorage.getItem(key));
+    } catch (e) {
+      console.error('Error getting data from sessionStorage', e);
+      return null;
+    }
+  }
+
+}
