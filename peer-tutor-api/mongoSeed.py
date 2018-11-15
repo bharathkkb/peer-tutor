1f9c9bf3ff244ac1f0f5a267ddf23ba3393a1da9
SheldonYL <liyanpeng2604@hotmail.com>
2018-11-14 20:55:30 -0800

    rebased

--- peer-tutor-api/mongoSeed.py
+++ peer-tutor-api/mongoSeed.py
@@ -15,8 +15,8 @@ student2 = {"name": "Albert", "student_id": "04",
             "username": "albert@gmail.com", "password": "pass123"}
 student3 = {"name": "Sheldon", "student_id": "06",
             "username": "sheldon@gmail.com", "password": "sheldon123"}
-studentCol.insert_one(student1)	studentCol.insert_one(student1)
-studentCol.insert_one(student2)	studentCol.insert_one(student2)
+studentCol.insert_one(student1)
+studentCol.insert_one(student2)
 studentCol.insert_one(student3)
 
 meetingCol = mydb["meeting"]
