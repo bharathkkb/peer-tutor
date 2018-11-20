83fb1e7dc9b753c3d64be49a542e22e0d5fff4b7
firen777 <hivemindac1995@gmail.com>
2018-11-20 15:05:24 -0800

    small commit to save

--- peer-tutor-ui/angular/src/app/_guards/auth.guard.spec.ts
+++ peer-tutor-ui/angular/src/app/_guards/auth.guard.spec.ts
@@ -3,16 +3,29 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { AuthGuard } from './auth.guard';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LocalStorageService } from '../_services';
 
 describe('AuthGuard', () => {
+
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      providers: [AuthGuard]
+      providers: [
+        AuthGuard,
+        LocalStorageService,
+      ]
     });
   });
 
   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
+
+  it('should not be able to hit route when localStorage does not have user', 
+    inject([AuthGuard], (guard: AuthGuard) => {
+    
+  }));
+
 });
