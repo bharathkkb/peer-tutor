cc34924a3f2d4ec6b3bd9b6c173d38e156e3951b
firen777 <hivemindac1995@gmail.com>
2018-11-20 21:15:09 -0800

    Test wb testing (#68)
    * small commit to save
    
    * login with mock auth service pass, auth guard test pass

--- peer-tutor-ui/angular/src/app/_guards/auth.guard.spec.ts
+++ peer-tutor-ui/angular/src/app/_guards/auth.guard.spec.ts
@@ -1,18 +1,37 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
-import { Router } from '@angular/router';
+import { Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
+let mockSnapshot:any = jasmine.createSpyObj<RouterStateSnapshot>("RouterStateSnapshot", ['toString']);
+
 describe('AuthGuard', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      providers: [AuthGuard]
+      providers: [
+        AuthGuard,
+        {provide: RouterStateSnapshot, useValue: mockSnapshot},
+      ]
     });
   });
 
   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
+
+  it('should not be able to hit route when localStorage does not have user', 
+    inject([AuthGuard], (guard: AuthGuard) => {
+      localStorage.clear();
+      expect(guard.canActivate(new ActivatedRouteSnapshot(), mockSnapshot)).toBe(false);
+  }));
+
+  it('should be able to hit route when localStorage have user', 
+    inject([AuthGuard], (guard: AuthGuard) => {
+      localStorage.setItem('currentUser', "{}");
+      expect(guard.canActivate(new ActivatedRouteSnapshot(), mockSnapshot)).toBe(true);
+  }));
+
 });
--- peer-tutor-ui/angular/src/app/login/login.component.spec.ts
+++ peer-tutor-ui/angular/src/app/login/login.component.spec.ts
@@ -1,10 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { AuthenticationService } from '../_services';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http/';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
+
+class MockAuthService extends AuthenticationService{
+  login(username: string, password: string) {
+    if (username=="test1@gmail.com" && password=="password1") {return of(1);} //success
+    else return throwError(new Error("err"));
+  }
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -13,14 +21,14 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      providers: [
-        AuthenticationService
-      ],
       imports: [ 
         FormsModule,
         HttpClientModule,
         RouterTestingModule,
       ],
+      providers: [
+        {provide: AuthenticationService, useClass: MockAuthService},
+      ]
     })
     .compileComponents();
   }));
@@ -33,6 +41,25 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check login success: "submitted" should be true, "loginError" should be false, "loginErrorMsg" should be falsy', 
+    inject([AuthenticationService],(authservice:AuthenticationService)=>{
+    component.loginUsername = "test1@gmail.com";
+    component.loginPassword = "password1"
+    component.tryLogin();
+    expect(component.submitted).toBe(true);
+    expect(component.loginError).toBe(false);
+    expect(component.loginErrorMsg).toBeFalsy();
+  }));
+
+  it('should check login failure even username match but password not match: "submitted" should be true, "loginError" should be true, "loginErrorMsg" should be "Email or Password not matched!"', ()=>{
+    component.loginUsername = "test1@gmail.com";
+    component.loginPassword = "password1X"
+    component.tryLogin();
+    expect(component.submitted).toBe(true);
+    expect(component.loginError).toBe(true);
+    expect(component.loginErrorMsg).toBe("Email or Password not matched!");
   });
 
 });
