ad3f7d7730b4082ebc2ff9aff4ee339ecf9749c4
bharathkkb <bharathkrishnakb@gmail.com>
2018-11-22 05:31:38 -0800

    fix path

--- peer-tutor-api/wb-unittests/unittest_runner_html.py
+++ peer-tutor-api/wb-unittests/unittest_runner_html.py
@@ -2,10 +2,12 @@ import os
 import unittest
 import xmlrunner
 import HTMLTestRunner
-
+import pathlib
 
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
+    if not os.path.exists("reports"):
+        os.makedirs("reports")
     try:
         file = open("reports/index.html", 'r')
     except IOError:
