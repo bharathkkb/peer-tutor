d14868bc4d8ed45f521a0075b3f33f9bf08b6d60
bharathkkb <bharathkrishnakb@gmail.com>
2018-11-23 01:50:11 -0800

    add avg ratings-2

--- peer-tutor-api/test_api.py
+++ peer-tutor-api/test_api.py
@@ -672,7 +672,7 @@ def test_get_rating_by_id(url):
     assert data["comment"] == "02 to 04 Very good comment"
     assert data["rating_score"] == "4"
 
-# check get rating by id
+# check get avg rating by id
 
 
 def test_get_avg_rating_by_id(url):
