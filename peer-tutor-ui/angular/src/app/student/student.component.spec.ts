13ba0414815a7d4d709b5718bc6ab4ac53c403e6
firen777 <hivemindac1995@gmail.com>
2018-11-26 16:22:57 -0800

    retrieve data works

--- peer-tutor-ui/angular/src/app/student/student.component.spec.ts
+++ peer-tutor-ui/angular/src/app/student/student.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StudentComponent } from './student.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('StudentComponent', () => {
   let component: StudentComponent;
@@ -8,7 +10,11 @@ describe('StudentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StudentComponent ]
+      declarations: [ StudentComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+      ]
     })
     .compileComponents();
   }));
--- peer-tutor-ui/angular/src/app/student/student.component.ts
+++ peer-tutor-ui/angular/src/app/student/student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LocalStorageService, CURRENT_USER, UserService, RatingDataService } from '../_services';
+import { Student, Rating } from '../_models';
 
 @Component({
   selector: 'app-student',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  student_id$: string = "";
+  self_flag$: boolean = false;
+
+  studentObj$: Student;
+  studentRatings$: Rating[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private localStorageService:LocalStorageService,
+    private ratingDataService:RatingDataService,
+    private userService: UserService,
+  ) 
+  { 
+    this.route.params.subscribe(param=>{
+      this.student_id$ = param["id"];
+      //check whether it's current user's self profile
+      if (this.student_id$) {
+        this.self_flag$ = this.localStorageService.getCurrentUser()[CURRENT_USER.student_id.key] === this.student_id$
+      }
+      else {
+        this.student_id$ = this.localStorageService.getCurrentUser()[CURRENT_USER.student_id.key]
+        this.self_flag$ = true;
+      }
+    })
+  }
 
   ngOnInit() {
+    this.userService.getByStudentId(this.student_id$).subscribe(
+      student=>{this.studentObj$ = student},
+      err=>{console.log(err)}
+    )
+    this.ratingDataService.getRatingsByReceivedStudentId(this.student_id$).subscribe(
+      ratings=>{this.studentRatings$ = ratings},
+      err=>{console.log(err)}
+    )
   }
 
 }
